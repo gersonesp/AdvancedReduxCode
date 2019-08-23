092bfaa161618700be4c382a3a2befe3815578ea
Gerson Espinal <gerson.espinal@gmail.com>
2019-08-23 13:43:59 -0400

    Add redux and updated tests to work with redux by implementing root component

--- prod/testing/src/components/CommentBox.js
+++ prod/testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class CommentBox extends Component {
   state = {
@@ -13,6 +15,8 @@ class CommentBox extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
 
     this.setState({
       comment: ""
@@ -34,4 +38,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);
