74f65ee3406ab2df3c667f8845fb46988d7ad005
Gerson Espinal <gerson.espinal@gmail.com>
2019-08-27 19:38:50 -0400

    Add React-router-dom

--- prod/testing/src/index.js
+++ prod/testing/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Root from "root";
 
 import App from "components/App";
 
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>,
   document.querySelector("#root")
 );
