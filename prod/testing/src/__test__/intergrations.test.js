fff76a8028bf221c8057c05cc6fe97313a5ac640
Gerson Espinal <gerson.espinal@gmail.com>
2019-08-27 12:00:43 -0400

    Finish testing

--- prod/testing/src/__test__/intergrations.test.js
+++ prod/testing/src/__test__/intergrations.test.js
@@ -28,12 +28,12 @@ it("can fetch a list of comments and display them", done => {
   wrapped.find(".fetch-comments").simulate("click");
 
   // Expect to find a list of comments
-  setTimeout(() => {
+  moxios.wait(() => {
     wrapped.update();
 
     expect(wrapped.find("li").length).toEqual(2);
 
     done();
     wrapped.unmount();
-  }, 100);
+  });
 });
