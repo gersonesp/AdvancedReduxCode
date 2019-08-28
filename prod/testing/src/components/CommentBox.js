4cb9f4e8b0148ee33d34e1f0c2459981a59f7a6b
Gerson Espinal <gerson.espinal@gmail.com>
2019-08-27 20:27:52 -0400

    Add auth logic

--- prod/testing/src/components/CommentBox.js
+++ prod/testing/src/components/CommentBox.js
@@ -7,6 +7,20 @@ class CommentBox extends Component {
     comment: ""
   };
 
+  componentDidMount() {
+    this.shouldNavigateAway();
+  }
+
+  componentDidUpdate() {
+    this.shouldNavigateAway();
+  }
+
+  shouldNavigateAway() {
+    if (!this.props.auth) {
+      this.props.history.push("/");
+    }
+  }
+
   handleChange = event => {
     this.setState({
       comment: event.target.value
@@ -43,7 +57,11 @@ class CommentBox extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(CommentBox);
